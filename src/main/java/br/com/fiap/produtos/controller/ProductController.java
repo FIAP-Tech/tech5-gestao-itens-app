5d2b3c34637503c99b805b9c7b9440f34f65e4f3
devops.jeffersantos <devops.jeffersantos@gmail.com>
2024-07-02 21:37:03 -0300

    change config for spring security!

--- src/main/java/br/com/fiap/produtos/controller/ProductController.java
+++ src/main/java/br/com/fiap/produtos/controller/ProductController.java
@@ -6,6 +6,7 @@ import br.com.fiap.produtos.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,12 +26,14 @@ public class ProductController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('client_admin')")
     public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
         return productService.createProduct(productRequest);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasAnyRole('client_admin','client_user')")
     public List<ProductResponse> getAllProducts() {
         return productService.getAllProducts();
 
@@ -38,24 +41,28 @@ public class ProductController {
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasAnyRole('client_admin','client_user')")
     public ProductResponse getByIdProduct(@PathVariable("id") Long id) {
         return productService.getProductById(id);
     }
 
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('client_admin')")
     public String updateProduct(@PathVariable("id") Long id, @RequestBody ProductRequest productRequest) {
         return productService.updateProduct(id, productRequest);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasRole('client_admin')")
     public void deleteProduct(@PathVariable("id") Long id) {
         productService.deleteProduct(id);
 
     }
 
     @PostMapping("/{idProduto}/atualizar-estoque")
+    @PreAuthorize("hasRole('client_admin')")
     public ResponseEntity<String> atualizarEstoqueProduto(@PathVariable Long idProduto, @RequestParam Integer quantidadeVendida) {
         return productService.atualizarEstoqueProduto(idProduto, quantidadeVendida)
                 .map(order -> ResponseEntity.ok("Estoque atualizado com Sucesso"))
