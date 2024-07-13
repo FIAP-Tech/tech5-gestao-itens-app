4d8c4d7be5eb5edbd67eeaea97249c2cd141c079
Ricardo Rafael Lara Gomes <ricardolara.ti@gmail.com>
2024-07-12 23:04:29 -0300

    melhorias

---
+++ src/main/java/br/com/fiap/produtos/exceptionhandler/CustomExceptionHandler.java
@@ -0,0 +1,55 @@
+package br.com.fiap.produtos.exceptionhandler;
+
+import io.jsonwebtoken.MalformedJwtException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    private final ErrorMessage errorMessage = new ErrorMessage();
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorMessage> illegalArgument(IllegalArgumentException e, HttpServletRequest request){
+        var status = HttpStatus.BAD_REQUEST;
+
+        errorMessage.setTimestamp(LocalDateTime.now());
+        errorMessage.setStatus(status.value());
+        errorMessage.setMessage(e.getMessage());
+        errorMessage.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(this.errorMessage);
+    }
+
+    @ExceptionHandler(DateTimeParseException.class)
+    public ResponseEntity<ErrorMessage> dateTimeParseException(DateTimeParseException e, HttpServletRequest request){
+        var status = HttpStatus.BAD_REQUEST;
+
+        errorMessage.setTimestamp(LocalDateTime.now());
+        errorMessage.setStatus(status.value());
+        errorMessage.setMessage(e.getMessage());
+        errorMessage.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(this.errorMessage);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorMessage> accessDeniedException(AccessDeniedException e, HttpServletRequest request){
+        var status = HttpStatus.UNAUTHORIZED;
+
+        errorMessage.setTimestamp(LocalDateTime.now());
+        errorMessage.setStatus(status.value());
+        errorMessage.setMessage("Acesso negado: Você não tem permissão para acessar este recurso.");
+        errorMessage.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(this.errorMessage);
+    }
+
+}
